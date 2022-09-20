922f54cf61963addfdf13757b0ae1b67bfd4234f
nrissman <nrissman90@gmail.com>
2022-09-20 10:35:38 -0400

    HTML and CSS complete

---
+++ script.js
@@ -0,0 +1,5 @@
+//VARIABLES
+
+//if you add a comma after the first variable you dont have to repeatidly put const in front 
+const modal = document.querySelector(".modal"),
+    btn = document.querySelector()
