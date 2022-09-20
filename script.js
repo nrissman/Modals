7d998086ca54b00d6693e0441fb42d55bdfc65cd
nrissman <nrissman90@gmail.com>
2022-09-20 11:03:27 -0400

    finished

--- script.js
+++ script.js
@@ -2,4 +2,24 @@
 
 //if you add a comma after the first variable you dont have to repeatidly put const in front 
 const modal = document.querySelector(".modal"),
-    btn = document.querySelector()
+    btn = document.querySelector(".btn")
+    close = document.querySelector(".close")
+    modalContent =document.querySelector(".modal-content")
+
+btn.addEventListener("click", openModal)
+close.addEventListener("click", closeModal)
+modal.addEventListener("click", closeModal)
+
+//OPEN MODAL
+function openModal(e) {
+    e.preventDefault()
+    modal.style.display ="block"
+}
+//CLSOE MODAL
+function closeModal(e) {
+    modalContent.classList.add("slide-up")
+    setTimeout(() => {
+        modal.style.display ="none"
+        modalContent.classList.remove("slide-up")
+    }, 500)
+}
